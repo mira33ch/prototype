139525525e38608bd769492f60c5f99d573d7df1
MariemChaabane <145264406+mira33ch@users.noreply.github.com>
2026-01-20 21:50:28 +0100

    Update formation.service.ts

--- portfolio-frontend/src/app/services/formation.service.ts
+++ portfolio-frontend/src/app/services/formation.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class FormationService {
-  private apiUrl = 'http://localhost:8080/portfolio/formation';
+  private apiUrl = '/api/portfolio/formation';
 
   constructor(private http: HttpClient) {}
 
