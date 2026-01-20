707eeb822bf686b0c402e2826e526eb31f40f93d
MariemChaabane <145264406+mira33ch@users.noreply.github.com>
2026-01-20 21:50:57 +0100

    Update skill.service.ts

--- portfolio-frontend/src/app/services/skill.service.ts
+++ portfolio-frontend/src/app/services/skill.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class SkillService {
-  private apiUrl = 'http://localhost:8080/portfolio/skill';
+  private apiUrl = '/api/portfolio/skill';
 
   constructor(private http: HttpClient) {}
 
